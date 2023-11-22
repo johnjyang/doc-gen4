8d5df4df44a0b824f2117fda484cc880466a4d9d
johnjyang <yang.jianang@gmail.com>
2023-11-21 21:40:31 -0500

    wip: change paths

--- DocGen4/Output/Navbar.lean
+++ DocGen4/Output/Navbar.lean
@@ -58,46 +58,6 @@ The main entry point to rendering the navbar on the left hand side.
 def navbar : BaseHtmlM Html := do
   pure
     <html lang="en">
-      <head>
-        [← baseHtmlHeadDeclarations]
-
-        <script type="module" src={s!"{← getRoot}nav.js"}></script>
-        <script type="module" src={s!"{← getRoot}color-scheme.js"}></script>
-        <base target="_parent" />
-      </head>
-
-      <body>
-        <div class="navframe">
-        <nav class="nav">
-          <h3>General documentation</h3>
-          <div class="nav_link"><a href={s!"{← getRoot}"}>index</a></div>
-          <div class="nav_link"><a href={s!"{← getRoot}foundational_types.html"}>foundational types</a></div>
-          /-
-          TODO: Add these in later
-          <div class="nav_link"><a href={s!"{← getRoot}tactics.html"}>tactics</a></div>
-          <div class="nav_link"><a href={s!"{← getRoot}commands.html"}>commands</a></div>
-          <div class="nav_link"><a href={s!"{← getRoot}hole_commands.html"}>hole commands</a></div>
-          <div class="nav_link"><a href={s!"{← getRoot}attributes.html"}>attributes</a></div>
-          <div class="nav_link"><a href={s!"{← getRoot}notes.html"}>notes</a></div>
-          <div class="nav_link"><a href={s!"{← getRoot}references.html"}>references</a></div>
-          -/
-          <h3>Library</h3>
-          {← moduleList}
-          <div id="settings" hidden="hidden">
-            -- `input` is a void tag, but can be self-closed to make parsing easier.
-            <h3>Color scheme</h3>
-            <form id="color-theme-switcher">
-                <label for="color-theme-dark">
-                    <input type="radio" name="color_theme" id="color-theme-dark" value="dark" autocomplete="off"/>dark</label>
-                <label for="color-theme-system" title="Match system theme settings">
-                    <input type="radio" name="color_theme" id="color-theme-system" value="system" autocomplete="off"/>system</label>
-                <label for="color-theme-light">
-                    <input type="radio" name="color_theme" id="color-theme-light" value="light" autocomplete="off"/>light</label>
-            </form>
-          </div>
-        </nav>
-        </div>
-      </body>
     </html>
 
 end Output
